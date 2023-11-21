102743aa4a1599885453afb3148b7902b72b9ec6
wangyufsn <dev.wangyu@proton.me>
2023-11-21 14:11:07 +0800

    up

--- run.proxy.sh
+++ run.proxy.sh
@@ -3,6 +3,9 @@ rm -rvf el-cl-genesis-data
 
 
 docker run \
+  -e http_proxy=http://host.docker.internal:7890 \
+  -e https_proxy=http://host.docker.internal:7890 \
+  -e all_proxy=socks5://host.docker.internal:7890 \
   --rm -it -v $PWD/el-cl-genesis-data:/data \
   -v $PWD/config-example:/config \
   wangyufsn/ethereum-genesis-generator:2.0.4 all
