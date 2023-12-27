c311cb2acb2e2911f8644b59892beb71cbc8c12a
lyfsn <dev.wangyu@proton.me>
2023-12-27 15:24:49 +0800

    fix build shell

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 
 docker rmi lyfsn/ethereum-genesis-generator:2.0.4  
 
-docker build -f Proxy.Dockerfile  -t lyfsn/ethereum-genesis-generator:2.0.4 .
+docker build -f Dockerfile  -t lyfsn/ethereum-genesis-generator:2.0.4 .
